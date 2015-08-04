c6004adacc33cfc2594ebfc7e0723449017e27d5
Jesse O'Neill-Oine <jesse@smartthings.com>
2015-08-04 11:19:07 -0500

    Updated meta-data for hue bulbs to use multi-attribute tile for 6x4 display

--- devicetypes/smartthings/hue-bulb.src/hue-bulb.groovy
+++ devicetypes/smartthings/hue-bulb.src/hue-bulb.groovy
@@ -22,41 +22,32 @@ metadata {
 	simulator {
 		// TODO: define status and reply messages here
 	}
+	
+	tiles(scale: 2) {
+		multiAttributeTile(name:"rich-lighting-control", type:"lighting", width: 6, height: 4) {
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#79b821", nextState:"turningOff"
+				attributeState "off", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff", nextState:"turningOn"
+				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#79b821", nextState:"turningOff"
+				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff", nextState:"turningOn"
+			}
+			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+				attributeState "level", action:"switch level.setLevel"
+			}
+			tileAttribute ("device.color", key: "COLOR_CONTROL") {
+				attributeState "color", action:"setAdjustedColor"
+			}
+		}
+		standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+		standardTile("reset", "device.reset", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+			state "default", label:"Reset Color", action:"reset", icon:"st.lights.philips.hue-single"
+		}
 
-	standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-		state "on", label:'${name}', action:"switch.off", icon:"st.lights.philips.hue-single", backgroundColor:"#79b821"
-		state "off", label:'${name}', action:"switch.on", icon:"st.lights.philips.hue-single", backgroundColor:"#ffffff"
+		main(["rich-lighting-control"])
+		details(["rich-lighting-control", "refresh", "reset"])
 	}
-	standardTile("reset", "device.reset", inactiveLabel: false, decoration: "flat") {
-		state "default", label:"Color Reset", action:"reset", icon:"st.lights.philips.hue-single"
-	}
-	standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
-		state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-	}
-	controlTile("rgbSelector", "device.color", "color", height: 3, width: 3, inactiveLabel: false) {
-		state "color", action:"setAdjustedColor"
-	}
-	controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false, range:"(0..100)") {
-		state "level", action:"switch level.setLevel"
-	}
-	valueTile("level", "device.level", inactiveLabel: false, decoration: "flat") {
-		state "level", label: 'Level ${currentValue}%'
-	}
-	controlTile("saturationSliderControl", "device.saturation", "slider", height: 1, width: 2, inactiveLabel: false) {
-		state "saturation", action:"color control.setSaturation"
-	}
-	valueTile("saturation", "device.saturation", inactiveLabel: false, decoration: "flat") {
-		state "saturation", label: 'Sat ${currentValue}    '
-	}
-	controlTile("hueSliderControl", "device.hue", "slider", height: 1, width: 2, inactiveLabel: false) {
-		state "hue", action:"color control.setHue"
-	}
-	valueTile("hue", "device.hue", inactiveLabel: false, decoration: "flat") {
-		state "hue", label: 'Hue ${currentValue}   '
-	}
-
-	main(["switch"])
-	details(["switch", "levelSliderControl", "rgbSelector", "refresh", "reset"])
 
 }
 
